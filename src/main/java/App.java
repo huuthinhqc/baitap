112e5e73d9aab797da401a8359c50e2ee441dabf
thinhqc <thinh.trinh@onemount.com>
2021-08-05 19:32:44 +0700

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,82 @@
+public class App {
+
+    public static void main(String[] args) {
+//         bài 4
+        int year = 1993;
+        int age = 2021 - year;
+        String str = String.format("Tuổi của người sinh năm 1993 là: %d ", age);
+        System.out.println(str);
+
+//         bài 5
+//        double a = 5.5;
+//        double b = 10;
+//        double t1 = a + b;
+//        double t2 = a - b;
+//        double t3 = a * b;
+//        double t4 = a / b;
+//        String str1 = String.format("Tổng của 2 số a và b là: %.3f", t1);
+//        String str2 = String.format("Hiệu của 2 số a và b là: %.3f", t2);
+//        String str3 = String.format("Tích của 2 số a và b là: %.3f", t3);
+//        String str4 = String.format("Thương của 2 số a và b là: %.3f", t4);
+//        System.out.println(str1);
+//        System.out.println(str2);
+//        System.out.println(str3);
+//        System.out.println(str4);
+
+//         bài 6
+//        String a = "Product";
+//        int soluong = 100;
+//        int dongia = 5000;
+//        double tilethue = 0.1;
+//        String ten = String.format("Sản phẩm: %s", a);
+//        String tien = String.format("Số tiền phải trả là: %d", soluong * dongia);
+//        String thue = String.format("Số tiền thuế là: %.0f", soluong * dongia * tilethue);
+//        System.out.println(ten);
+//        System.out.println(tien);
+//        System.out.println(thue);
+
+//         bài 7
+//        double diemtoan = 7;
+//        double diemly = 8.5;
+//        double diemhoa = 9.8;
+//        double diemtb = (diemtoan + diemhoa + diemly)/3 ;
+//        String a = String.format("Điểm toán = %.1f",diemtoan);
+//        String b = String.format("Điểm lý = %.1f",diemly);
+//        String c = String.format("Điểm hóa = %.1f",diemhoa);
+//        String d = String.format("Điểm trung bình của học sinh đó là: %.2f",diemtb );
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(c);
+//        System.out.println(d);
+
+
+        // bài 8
+//        double bankinh = 5.6;
+//        double chuvi = bankinh * 2 * Math.PI;
+//        double dientich = bankinh * bankinh * Math.PI;
+//        String a = String.format("Bán kính = %.1f cm", bankinh);
+//        String b = String.format("Chu vi của hình tròn là %.1f cm2",chuvi);
+//        String c = String.format("Diện tích của hình tròn là %.1f cm2",dientich);
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(c);
+
+        // bài 9
+
+
+
+        // bài 10
+//        int a = 10;
+//        int b = 20;
+//        String c = String.format("Số a là %d",a);
+//        String d = String.format("Số b là %d",b);
+//        String e = String.format("Max là số a = %d, Min là số b = %d",a,b);
+//        String f = String.format("Max là số b = %d, Min là số a = %d",b,a);
+//        System.out.println(c);
+//        System.out.println(d);
+//        if( a > b)
+//            System.out.println(e);
+//        else
+//            System.out.println(f);
+    }
+}
